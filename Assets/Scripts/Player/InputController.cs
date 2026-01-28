b9ff90d41b48e696782716c797532aaff861066d
LUOIAOSENG <3316211327@qq.com>
2026-01-28 21:55:14 +0800

    feat: InputController

---
+++ Assets/Scripts/Player/InputController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class InputController
+{
+    public static Vector2 MoveVector => new Vector2(
+        Input.GetAxis("Horizontal"),
+        Input.GetAxis("Vertical")
+     );
+    public static Vector2 MouseVector => new Vector2(
+        Input.GetAxis("Mouse X"),
+        Input.GetAxis("Mouse Y")
+    );
+    public static bool Fire1Down => Input.GetButtonDown("Fire1");
+    public static bool Fire2Down => Input.GetButtonDown("Fire2");
+    public static bool RedKeyDown => Input.GetKeyDown(KeyCode.R);
+    public static bool GreenKeyDown => Input.GetKeyDown(KeyCode.G);
+    public static bool BlueKeyDown => Input.GetKeyDown(KeyCode.B);
+}
