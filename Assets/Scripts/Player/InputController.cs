f2988370f4647db30dab5a2350f6ca3ecc43e038
CompassLuo <luoyoucheng991528@gmail.com>
2026-01-29 19:38:26 +0800

    feat: Dash

--- Assets/Scripts/Player/InputController.cs
+++ Assets/Scripts/Player/InputController.cs
@@ -17,4 +17,5 @@ public class InputController
     public static bool BlueKeyDown => Input.GetKeyDown(KeyCode.B);
     public static bool Jump => Input.GetKeyDown(KeyCode.W);
     public static bool Interact => Input.GetButtonDown("Jump");
+    public static bool Dash => Input.GetKey(KeyCode.LeftShift);
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
 
@@ -9,12 +11,24 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float speed = 5;
     [SerializeField] private float jumpForce = 8;
+    [SerializeField] private float dashDistance = 5;
     public bool grounded;
     public bool enableInput = true;
+    public int direction = 1;
+    public bool dashed = false;
     void Update()
     {
         if (!enableInput)
             return;
+        var moveVector = InputController.MoveVector;
+        if (moveVector.x > 0)
+        {
+            direction = 1;
+        }
+        else if (moveVector.x < 0)
+        {
+            direction = -1;
+        }
         grounded = Physics2D.OverlapCircle(transform.position, 0.01f, groundMask);
         if (InputController.Jump)
         {
@@ -29,7 +43,24 @@ public class PlayerMovement : MonoBehaviour
     }
     void Move()
     {
-        transform.position = transform.position + new Vector3(InputController.MoveVector.x * speed * Time.deltaTime, 0, 0);
+        if (InputController.Dash && !dashed)
+        {
+            Dash();
+        }
+        else
+        {
+            transform.position = transform.position + new Vector3(InputController.MoveVector.x * speed * Time.deltaTime, 0, 0);
+        }
+    }
+    async void Dash()
+    {
+        if (dashed)
+            return;
+        transform.DOMoveX(transform.position.x + dashDistance * direction, 0.3f)
+        .SetEase(Ease.Linear);
+        dashed = true;
+        await Task.Delay(1000);
+        dashed = false;
     }
     void Jump()
     {
