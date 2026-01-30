f1434c62f09f63aed8d4e3a12a0c3c758130e7ae
LUOIAOSENG <3316211327@qq.com>
2026-01-30 20:10:07 +0800

    refactor: 修改PlayerMovement的部分代码

--- Assets/Scripts/Gameplay/Lift.cs
+++ Assets/Scripts/Gameplay/Lift.cs
@@ -32,7 +32,7 @@ public class Lift : MonoBehaviour
         if (InputController.Interact && player && !isMoving)
         {
             var playerMovement = player.GetComponent<PlayerMovement>();
-            playerMovement.isInputEnabled = false;
+            playerMovement.enableInput = false;
             isMoving = true;
 
             var temp = floor + (dir == "up" ? 1 : -1);
@@ -51,7 +51,7 @@ public class Lift : MonoBehaviour
                 playerMovement.transform.DOMoveY(floorTransforms[floor].position.y, 2);
                 liftSprite.transform.DOMoveY(floorTransforms[floor].position.y + 1.5f, 2).OnComplete(() =>
                 {
-                    playerMovement.isInputEnabled = true;
+                    playerMovement.enableInput = true;
                     isMoving = false;
                 });
             });
